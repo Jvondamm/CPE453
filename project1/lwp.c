45af00eb81e5598674f53bf44d889f7883eae0b0
joshuavd <joshua.vondamm@packetcraft.com>
2022-01-13 09:50:17 -0800

    added new funcs and made some progress on new_lwp

--- project1/lwp.c
+++ project1/lwp.c
@@ -3,30 +3,53 @@
 #include <stdlib.h>
 
 
-unsigned long PID = 0;              /* PID                                      */
-lwp_procs = 0;                      /* the current number of LWPs               */
-extern int lwp_running = 0;         /* the index of the currently running LWP   */
+unsigned long PID = 0;   /* PID                                      */
+lwp_procs = 0;           /* the current number of LWPs               */
+lwp_running = 0;         /* the index of the currently running LWP   */
+/* do I need to malloc lwp_ptable with LWP_PROC_LIMIT here? */
 
-int new_lwp(lwpfun func, void * arg, size_t stack_size)
+int new_lwp(lwpfun func, void *arg, size_t stack_size)
 {
     if (lwp_procs == LWP_PROC_LIMIT) {
-        return -1
+        return -1;
     }
+
+    ptr_int_t * ebp, * esp;
 
     /* number of LWPs and PID will not necessarily match */
     lwp_procs++;
     PID++;
     lwp_running = lwp_procs;
     lwp_ptable[lwp_running].pid = PID;
-    lwp_ptable[lwp_running].stack = malloc(sizeof(int) * stack_size);
+    if (!(lwp_ptable[lwp_running].stack = malloc(sizeof(int) * stack_size)))
+        return -1;
     lwp_ptable[lwp_running].stacksize = stack_size;
 
-    esp
+    ebp = esp = lwp_ptable[lwp_running].stack + lwp_ptable[lwp_running].stacksize;
 
-    return lwp_ptable[lwp_running].pid;
+    *esp = arg;
+    esp--;
+    *esp = lwp_exit;
+    esp--;
+    *esp = func;
+    esp--;
+    *esp=NULL; /* bogus base pointer */
+    esp--;
+
+    esp -=6; /* push the bogus registers? */
+
+    lwp_ptable[lwp_running].sp = esp;
+
+    return lwp_running;
 }
 
-void lwp_exit();
+void lwp_exit() {
+    lwp_procs--;
+    if (lwp_procs <= 0) {
+        lwp_stop();
+    }
+    free(lwp_ptable[lwp_running].stack);
+}
 
 int lwp_getpid() {
    return lwp_ptable[lwp_running].pid;
@@ -35,14 +58,16 @@ int lwp_getpid() {
 void lwp_yield() {
    SAVE_STATE();
     /* TODO: who knows? */
+
+    RESTORE_STATE(); /* restore state of next process */
 }
 
-void lwp_start()
+void lwp_start() {
+    if (lwp_procs == 0) {
+        return;
+    }
+}
 
 void lwp_stop()
-{
-    /* TODO: Restore original stack pointer.*/
-    RESTORE_STATE();
-}
 
-void lwp_set_scheduler(schedfun sched);
+void lwp_set_scheduler(schedfun sched)
